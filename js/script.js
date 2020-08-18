5ab7c4b6b7d007f4d766b3b71cfcc14de1451b9b
mirkar99 <mirkar99@gmail.com>
2020-08-18 23:22:24 +0200

    3.0.0

--- js/script.js
+++ js/script.js
@@ -13,28 +13,53 @@ const endMenu = document.querySelector('.endMenu');
 const restetBtn=document.querySelector('.endMenu__button--reset');
 const goBackBtn=document.querySelector('.endMenu__button--goBack');
 
+const wordBase=[['C','H','L','E','B'],['L','I','Z','A','K'],['B','U','L','K','A'],['K','E','B','A','B']];
 const currentWord=[];
-const word=['C','H','L','E','B'];
+const word=[];
 
 
-const fromMenuToGame=function(){
-  header.style.top='0';
-  menu.classList.add('hidden');
-
-  main.style.display='flex';
-  viev.style.display='flex';
-  game.classList.remove("hidden");
-
+const changeMenuSection=function(a){
+  header.style.top=a;
+  menu.classList.toggle('hidden');
+ 
 };
+const changeGameSection=function(a,b,c,d){
+  game.classList.add("hidden");
+  main.style.display=a;
+  viev.style.display=a;
+  game.style.display=b;
+  game.style.height=c;
+  letterButtons.forEach((el)=>{
+    if(el.classList.contains('hidden')){
+      el.classList.remove('hidden');
+    }});
+  if(d==true){
+  game.classList.remove("hidden");
+  }
+}
+const changeEndMenu=function(a,b){
+  restetBtn.style.display=a;
+  goBackBtn.style.display=a;
+  endMenu.classList.add('hidden');
+  if(b==true){
+  endMenu.classList.remove('hidden');
+}}
+
 const wordToHtml = function(){
+  word.splice(0,word.length)
+  let random = Math.floor(Math.random() * wordBase.length);
+  wordBase[random].forEach((el,i)=>{
+    word.push(el);
+  })
+  currentWord.splice(0,currentWord.length)
+  vievText.textContent='';
   for(el of word){
     currentWord.push('_');
-  
     vievText.textContent+=`${currentWord[el.indexOf(el)]} `;
     console.log(el);
   }
 };
-const checkTheGame= function(callback){
+const checkTheGame= function(){
   letterButtons.forEach((el)=>{
   el.addEventListener('click',function(){
     el.classList.add('hidden');
@@ -43,7 +68,8 @@ const checkTheGame= function(callback){
       if(el.value==arr){
         currentWord[i]=arr;
         if(word.join(' ')==currentWord.join(' ')){
-        callback();
+          changeGameSection('none','block','0')
+          changeEndMenu('block',true);
         }
       }
       else{
@@ -57,15 +83,14 @@ const checkTheGame= function(callback){
   });
   });
 };
-const afterGameEnd=function(){
 
- game.style.display='none';
-  restetBtn.style.display='block';
-  goBackBtn.style.display='block';
-  endMenu.classList.remove('hidden');
-}
 menuBtn.addEventListener('click',()=>{
-  fromMenuToGame();
-  wordToHtml();
-  checkTheGame(afterGameEnd);
+  changeMenuSection('0'); changeGameSection('flex','block','90vh',true); wordToHtml(); checkTheGame();
 },false)
+restetBtn.addEventListener('click',()=>{
+  changeEndMenu('none'); changeGameSection('flex','block','90vh',true); wordToHtml(); checkTheGame();
+},false)
+goBackBtn.addEventListener('click',()=>{
+  changeEndMenu('none'); changeMenuSection('40%'); changeGameSection('none','block');
+},false)
+
