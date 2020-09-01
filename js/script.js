a53c174a3fb9f03de68a9fd30dc6b56d127bd58e
mirkar99 <mirkar99@gmail.com>
2020-09-01 18:22:26 +0200

    2.0.0

--- js/script.js
+++ js/script.js
@@ -1,96 +1,153 @@
 const header = document.querySelector('.header');
-
+//Menu Section
 const menu = document.querySelector('.menu');
 const menuBtn = document.querySelector('.menu__button');
-
-const game = document.querySelector('.game');
+//Main Element
 const main = document.querySelector('.main');
-const viev  = document.querySelector('.viev');
+const imgPanel =[...document.querySelectorAll('.panel__img')];
 const vievText  = document.querySelector('.viev__text');
-const letterButtons=[...document.querySelectorAll('.main__button')];
-
+const gameText = document.querySelector('.game__text');
+const letterButtons=[...document.querySelectorAll('.game__button')];
+//EndMenu section
 const endMenu = document.querySelector('.endMenu');
 const restetBtn=document.querySelector('.endMenu__button--reset');
 const goBackBtn=document.querySelector('.endMenu__button--goBack');
-
-const wordBase=[['C','H','L','E','B'],['L','I','Z','A','K'],['B','U','L','K','A'],['K','E','B','A','B']];
+//Elemets to game work
+const wordBase=[['H','A','P','P','Y'],['B','R','E','A','D'],['R','O','O','M'],['G','I','F','T']];
 const currentWord=[];
 const word=[];
 
-
-const changeMenuSection=function(a){
-  header.style.top=a;
+//Change section elements
+const changeMenuSection=function(headerTop){
+  header.style.top=headerTop;
   menu.classList.toggle('hidden');
  
-};
-const changeGameSection=function(a,b,c,d){
-  game.classList.add("hidden");
-  main.style.display=a;
-  viev.style.display=a;
-  game.style.display=b;
-  game.style.height=c;
+}; // string
+const changeGameSection=function(removeClassHidden){
+  main.classList.add("hidden");
+  imgPanel.forEach((el,i)=>{
+    if(i>0){
+      el.classList.add('hidden');
+    }
+  })
   letterButtons.forEach((el)=>{
     if(el.classList.contains('hidden')){
       el.classList.remove('hidden');
     }});
-  if(d==true){
-  game.classList.remove("hidden");
+  if(removeClassHidden==true){
+  main.classList.remove("hidden");
   }
-}
-const changeEndMenu=function(a,b){
-  restetBtn.style.display=a;
-  goBackBtn.style.display=a;
+};// boolen
+const changeEndMenu=function(removeClassHidden){
   endMenu.classList.add('hidden');
-  if(b==true){
+  if(removeClassHidden==true){
   endMenu.classList.remove('hidden');
-}}
-
+  }
+};// boolen
+//Game elements
 const wordToHtml = function(){
   word.splice(0,word.length)
   let random = Math.floor(Math.random() * wordBase.length);
   wordBase[random].forEach((el,i)=>{
     word.push(el);
   })
+
   currentWord.splice(0,currentWord.length)
   vievText.textContent='';
+
   for(el of word){
     currentWord.push('_');
     vievText.textContent+=`${currentWord[el.indexOf(el)]} `;
-    console.log(el);
   }
 };
-const checkTheGame= function(){
+const startTheGame=  function(){
+      let noLetter=0;
+      let mistake=0;
+      letterButtons.forEach((el)=>{
+        el.addEventListener('click',function(){
+          el.classList.add('hidden');
+          word.forEach((arr,i)=>{
+            if(el.value==arr){
+              noLetter=null;
+              currentWord[i]=arr;
+              if(word.join(' ')==currentWord.join(' ')){
+                    mistake=0;
+                    gameText.textContent='You Won'
+                    gameText.style.color='green';
+                    whenGamesEnds();
+              }
+            }else{
+                if(noLetter!==null){
+                noLetter++;
+                }
+                if(noLetter==word.length){
+                  mistake+=1;
+                 if(mistake==1){
+                  imgPanel[1].classList.remove('hidden');
+                 }else if(mistake==2){
+                  imgPanel[2].classList.remove('hidden');
+                 }else if(mistake==3){
+                  imgPanel[3].classList.remove('hidden');
+                }else if(mistake==4){
+                  imgPanel[4].classList.remove('hidden');
+                }else if(mistake==5){
+                  imgPanel[5].classList.remove('hidden');
+                }else if(mistake==6){
+                  imgPanel[6].classList.remove('hidden');
+                }else if(mistake==7){
+                  imgPanel[7].classList.remove('hidden');
+                }else if(mistake==8){
+                  imgPanel[8].classList.remove('hidden');
+                  mistake=0;
+                  gameText.textContent='You Lose'
+                  gameText.style.color='red';
+                  whenGamesEnds();
+                }
+              }
+            }
+          });
+          noLetter=0;
+          vievText.textContent=`${currentWord.join(' ')} `;
+        });
+        });
+};
+const whenGamesEnds=function(){
   letterButtons.forEach((el)=>{
-  el.addEventListener('click',function(){
-    el.classList.add('hidden');
-    let a=0;
-    word.forEach((arr,i)=>{
-      if(el.value==arr){
-        currentWord[i]=arr;
-        if(word.join(' ')==currentWord.join(' ')){
-          changeGameSection('none','block','0')
-          changeEndMenu('block',true);
-        }
-      }
-      else{
-        a++
-          if(a==word.length){
-            console.log('żle');
-          }
-      }
+      el.classList.add('hidden');
     });
-    vievText.textContent=`${currentWord.join(' ')} `;
-  });
-  });
+  gameText.classList.remove('hidden');
+  setTimeout(()=>{
+    changeGameSection()
+    changeEndMenu(true);
+    gameText.classList.add('hidden');
+  },2000)
+};
+//Button's functions
+const whenMenuBtnClicked=  function(){
+  changeMenuSection('0'); 
+  changeGameSection(true); 
+  wordToHtml();
+};
+const whenResetBtnClicked=  function(){
+  changeEndMenu(false); 
+  changeGameSection(true); 
+  wordToHtml();
+};
+const whenGoBackBtnClicked = function(){
+  changeEndMenu(); 
+  changeMenuSection('40%'); 
+  changeGameSection();
 };
 
+startTheGame();
+//Button's with events and  functions
 menuBtn.addEventListener('click',()=>{
-  changeMenuSection('0'); changeGameSection('flex','block','90vh',true); wordToHtml(); checkTheGame();
+  whenMenuBtnClicked();
 },false)
 restetBtn.addEventListener('click',()=>{
-  changeEndMenu('none'); changeGameSection('flex','block','90vh',true); wordToHtml(); checkTheGame();
+  whenResetBtnClicked();
 },false)
 goBackBtn.addEventListener('click',()=>{
-  changeEndMenu('none'); changeMenuSection('40%'); changeGameSection('none','block');
+  whenGoBackBtnClicked();
 },false)
 
